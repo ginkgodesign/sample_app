8c15c2fef2ab0e2e02755f92456331294c14fd8f
Josh Hill <josh@ginkgodesign.co.uk>
2010-11-04 16:23:16 +0000

    Finished Chapter 8 exercises

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,60 +2,56 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
-  
-  before(:each) do
-    @base_title = "Ruby on Rails Tutorial Sample App | "
-  end
 
   describe "GET 'home'" do
     it "should be successful" do
-      get 'home'
+      get :home
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'home'
+      get :home
       response.should have_selector("title",
-                                    :content => @base_title + "Home")
+                                    :content => "Home")
     end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
-      get 'contact'
+      get :contact
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'contact'
+      get :contact
       response.should have_selector("title",
-                                    :content => @base_title + "Contact")
+                                    :content => "Contact")
     end
   end
   
   describe "GET 'about'" do
     it "should be successful" do
-      get 'about'
+      get :about
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'about'
+      get :about
       response.should have_selector("title",
-                                    :content => @base_title + "About")
+                                    :content => "About")
     end
   end
   
   describe "GET 'help'" do
     it "should be successful" do
-      get 'help'
+      get :help
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'help'
+      get :help
       response.should have_selector("title",
-                                    :content => @base_title + "Help")
+                                    :content => "Help")
     end
   end
 
